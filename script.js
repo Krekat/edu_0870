713ad81d95708429b5c64a80c52a2742688ad0a6
Krekat <71063580+Krekat@users.noreply.github.com>
2020-09-13 18:20:42 +0300

    11.09.2020

--- script.js
+++ script.js
@@ -5,28 +5,46 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
+let yandexInput = document.getElementsByName('text')[0];
 let keywords = ['Гобой','Как звучит флейта', 'Кларнет','Саксофон','Тромбон','Валторна'];
-let keyword = keywords[getRandom(0,keywords.length)];
+let keyword = keywords[getRandom(0,keywords.length-1)];
 let btn = document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited')[0];
+let nextPage = document.getElementsByClassName('pager__item_kind_next')[0];
+let i=0;
+let links = document.links;
 
-if (btn != undefined){
-    document.getElementsByName('text')[0].value = keyword;
-    document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited')[0].click();
+if (window.location.href.indexOf('yandex.ru') != -1){
+    if (btn != undefined){
+        let timerId = setInterval(()=>{
+            yandexInput.value += keyword[i];
+            i++;
+        if (i==keyword.length){
+            clearInterval(timerId);
+            btn.click();
+        }
+    },1000);
 }else{
-    let links = document.links;
-
-    for(let i=0; i<links.length; i++){
+    let nextYandexPage = true;
+    for(i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
-            console.log("Ссылка найдена "+links[i]);
+            nextYandexPage = false;
+            links[i].removeAttribute('target');
+            setTimeout(()=>{links.click();},getRandom(1000,4000));
             links[i].click();
             break;
         }
     }
+    if (nextYandexPage && window.location.href.indexOf('&p=9&') == -1){
+        setTimeout(()=>{nextPage.click();},getRandom(1000,4000));
+    }
 }
-
+}else{
+    setInterval(()=>{location.href = 'https://yandex.ru/';},getRandom(3000,7000));
+}
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
